25580141b71cac9183e9217d2e77cdc744d74e2f
nairec <irecgc@gmail.com>
2025-02-23 18:49:42 +0100

    updated api endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
     try {
       setIsLoading(true);
       
-      const response = await fetch('http://localhost:8000/analyze-content', {
+      const response = await fetch('https://restudy.onrender.com/analyze-content', {
         method: 'POST',
         body: formData,
       });
