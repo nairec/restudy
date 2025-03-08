0828e08cdab524bfed9b2d745aeb31295c6f1145
nairec <irecgc@gmail.com>
2025-03-08 16:44:04 +0100

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     try {
       setIsLoading(true);
       
-      const response = await fetch('https://restudy.onrender.com/analyze-content', {
+      const response = await fetch('https://restudyserver.onrender.com/analyze-content', {
         method: 'POST',
         body: formData,
       });
